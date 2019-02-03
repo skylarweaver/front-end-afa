bc375bcae2e53c6516ee78b51f2028571f01c416
Skylar Weaver <sweaver@deloitte.com>
2019-02-03 17:09:53 -0500

    Fixing synax issues in home-page

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -5,7 +5,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 
-export const AboutPageTemplate = ({ title, content, contentComponent }) => {
+export const HomePageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   const HeroContainer = styled.div`
@@ -72,18 +72,18 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   )
 }
 
-AboutPageTemplate.propTypes = {
+HomePageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
   content: PropTypes.string,
   contentComponent: PropTypes.func,
 }
 
-const AboutPage = ({ data }) => {
+const HomePage = ({ data }) => {
   const { markdownRemark: post } = data
 
   return (
     <Layout>
-      <AboutPageTemplate
+      <HomePageTemplate
         contentComponent={HTMLContent}
         title={post.frontmatter.title}
         content={post.html}
@@ -92,14 +92,14 @@ const AboutPage = ({ data }) => {
   )
 }
 
-AboutPage.propTypes = {
+HomePage.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-export default AboutPage
+export default HomePage
 
-export const aboutPageQuery = graphql`
-  query AboutPage($id: String!) {
+export const homePageQuery = graphql`
+  query HomePage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {
