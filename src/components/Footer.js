30539cae3fc91557fd8687e5d9e3acabd32b7b4e
skylarweaver <weaver.skylar@gmail.com>
2019-04-19 11:52:34 -0400

    Added Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import { Flex, Box } from '@rebass/grid'
+
+const WhyTitle = styled.h2`
+	font-size: 38px;
+	font-weight: bold;
+	letter-spacing: -1.33px;
+	line-height: 50px;
+`
+
+const Footer = ({ className }) => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className={className}>
+      <Flex>
+        <Box width={4 / 12}>
+          <Flex flexDirection={'column'}>
+            &copy; {currentYear} Adventures for Alopecia
+            <Link >Privacy Policy</Link>
+          </Flex>
+        </Box>
+        <Box width={4 / 12}>
+          <Flex flexDirection={'column'}>
+            Get in touch!
+          <a href="mailto:info@projectAFA.org?subject=Adventures for Alopecia Inquiry" target="_blank">info@projectAFA.org</a>
+          </Flex>
+        </Box>
+        <Box width={4 / 12}>
+          Follow us! <br></br>
+          <Flex>
+            <Box >
+              <a href="https://www.instagram.com/sky_earth_water/" target="_blank">Instagram</a>
+            </Box>
+            <Box >
+              <a href="https://www.instagram.com/sky_earth_water/" target="_blank">Facebook</a>
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+    </div>
+  )
+}
+
+Footer.propTypes = {
+}
+
+export default Footer 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { StaticQuery, graphql } from "gatsby"
 
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import './all.sass'
 
 // Styles
@@ -30,22 +31,22 @@ const TemplateWrapper = ({ children }) => (
     render={data => (
       <LayoutContainer>
         <Helmet>
-          <html lang="en" style={{height: '100%'}}/>
+          <html lang="en" style={{ height: '100%' }} />
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
 
-	        <meta property="og:type" content="business.business" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+
+          <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+          <meta name="theme-color" content="#fff" />
+
+          <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-photo.jpg" />
-          <meta property="og:site-name" content={data.site.siteMetadata.title}/>
+          <meta property="og:site-name" content={data.site.siteMetadata.title} />
           <meta property="og:description" content="1 motorcycle. 16 countries. 17,000 miles to Patagonia. All for Alopecia awareness and support" />
 
         </Helmet>
@@ -53,6 +54,7 @@ const TemplateWrapper = ({ children }) => (
         <Content>
           {children}
         </Content>
+        <Footer />
       </LayoutContainer>
     )}
   />
