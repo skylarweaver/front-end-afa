b2bfcabe11811e4b894b5b2fb488512ffde3081c
skylarweaver <weaver.skylar@gmail.com>
2019-05-10 12:07:12 -0400

    Removed html.js GA tag code

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,40 +71,11 @@ module.exports = {
       // id: "GTM-PC4MVVM",
       id: process.env.GOOGLE_TAGMANAGER_TRACKING,
 
-  //     // Include GTM in development.
-  //     // Defaults to false meaning GTM will only be loaded in production.
-  //     includeInDevelopment: false,
-
-  //     // Specify optional GTM environment details.
-  //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_AUTH_STRING",
-  //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIROMENT_PREVIEW_NAME",
-  //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
     },
   },
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: process.env.GOOGLE_ANALYTICS_TRACKING,
-    //     // Puts tracking script in the head instead of the body
-    //     head: false,
-    //     // Setting this parameter is optional
-    //     // anonymize: true,
-    //     // Setting this parameter is also optional
-    //     // respectDNT: true,
-    //     // Avoids sending pageview hits from custom paths
-    //     // exclude: ["/preview/**", "/do-not-track/me/too/"],
-    //     // Enables Google Optimize using your container Id
-    //     // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-    //     // Enables Google Optimize Experiment ID
-    //     // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-    //     // Set Variation ID. 0 for original 1,2,3....
-    //     // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-    //     // Any additional create only fields (optional)
-    //     // sampleRate: 5,
-    //     // siteSpeedSampleRate: 10,
-    //     // cookieDomain: "example.com",
-    //   },
-    // },
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
