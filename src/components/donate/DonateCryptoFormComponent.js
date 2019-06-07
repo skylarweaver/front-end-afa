31cf32d671f37903ef31c9a71a59eb3e6784e73d
skylarweaver <weaver.skylar@gmail.com>
2019-06-06 21:38:05 -0400

    Adding QR Code to crypto

--- src/components/donate/DonateCryptoFormComponent.js
+++ src/components/donate/DonateCryptoFormComponent.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { cryptoDonationPropTypes } from '../../proptypes/donate-proptypes'
 import { Flex, Box } from '@rebass/grid'
 import MarkdownContent from '../MarkdownContent'
+import Bitcoin_Qr_Code from '../../img/qrCodes/bitcoin_qr_code.png'
 
 const StyledStepNumber = styled.h3`
   font-family: Vidaloka;
@@ -47,6 +48,7 @@ font-family: Roboto;
 background: ${props => props.active ? props.theme.tertiary : props.theme.primary};
 color: ${props => props.active ? props.theme.white : props.theme.white};
 border: 1px solid white;
+white-space: nowrap;
 &:focus {
   outline: none;
 }
@@ -55,6 +57,27 @@ border: 1px solid white;
   background: ${props => props.active ? props.theme.tertiary : props.theme.tertiaryLight};
   border: none;
 };
+`
+
+const QrButton = styled.button`
+font-family: Roboto;
+/* Adapt the colors based on primary prop */
+background: ${props => props.active ? props.theme.tertiary : props.theme.primary};
+color: ${props => props.active ? props.theme.white : props.theme.white};
+border: 1px solid white;
+white-space: nowrap;
+&:focus {
+  outline: none;
+}
+&:hover {
+  cursor: pointer;
+  background: ${props => props.active ? props.theme.tertiary : props.theme.tertiaryLight};
+  border: none;
+};
+`
+
+const QrCode = styled.img`
+
 `
 
 class DonateCryptoFormComponent extends React.Component {
@@ -67,6 +90,8 @@ class DonateCryptoFormComponent extends React.Component {
       canMakePayment: false,
       notes: '',
       anonymous: false,
+      newCopiedState: {},
+      newShowQrState: {},
     };
   }
 
@@ -96,14 +121,37 @@ class DonateCryptoFormComponent extends React.Component {
 
     const onAddressCopy = (name) => {
       const newCopiedState = {};
-      addressObjects.forEach(add => { // Rest copied to false for each state
-        if (add.name === name) {
-          newCopiedState[`${add.name}IsCopied`] = true;
+      const newShowQRState = {};
+      addressObjects.forEach(addr => { // Reset copied to false for each state
+        if (addr.name === name) {
+          newCopiedState[`${addr.name}IsCopied`] = true;
         } else {
-          newCopiedState[`${add.name}IsCopied`] = false;
+          newCopiedState[`${addr.name}IsCopied`] = false;
+          newShowQRState[`${addr.name}ShowQr`] = false;
         }
       })
-      this.setState(newCopiedState);
+      this.setState({
+        ...newCopiedState,
+        ...newShowQRState,
+      })
+    }
+    const onShowQr = (name) => {
+      console.log('name: ', name);
+      console.log('hiiiii');
+      const newCopiedState = {};
+      const newShowQRState = {};
+      addressObjects.forEach(addr => { // Reset copied to false for each state
+        if (addr.name === name) {
+          newShowQRState[`${addr.name}ShowQr`] = true;
+        } else {
+          newShowQRState[`${addr.name}ShowQr`] = false;
+        }
+        newCopiedState[`${addr.name}IsCopied`] = false;
+      })
+      this.setState({
+        ...newCopiedState,
+        ...newShowQRState,
+      });
     }
 
     const CryptoAddress = ({ name, address }) => {
@@ -113,9 +161,13 @@ class DonateCryptoFormComponent extends React.Component {
           <Flex>
             <StyledCryptoAddress>{address}</StyledCryptoAddress>
             <CopyToClipboard text={address} onCopy={() => onAddressCopy(name)}>
-              <CopyButton active={this.state[`${name}IsCopied`]}>{this.state[`${name}IsCopied`] ? 'Copied!' : 'Copy'}</CopyButton>
+              <CopyButton active={this.state[`${name}IsCopied`]}> {this.state[`${name}IsCopied`] ? 'Copied!' : 'Copy'} </CopyButton>
+            </CopyToClipboard>
+            <CopyToClipboard text="Please double check you copied the correct address." onCopy={() => onShowQr(name)}>
+              <QrButton active={this.state[`${name}ShowQr`]}> {this.state[`${name}ShowQr`] ? 'Show QR' : 'Show QR'} </QrButton>
             </CopyToClipboard>
           </Flex>
+          {this.state[`${name}ShowQr`] ? <QrCode src={Bitcoin_Qr_Code} /> : ''}
         </div>
       )
     }
@@ -134,21 +186,21 @@ class DonateCryptoFormComponent extends React.Component {
         <Flex my={[3, 3, 4]} alignItems='baseline'>
           <StyledStepNumber>2.</StyledStepNumber>
           <Box>
-          <StyledStep>
-            {/* {this.props.cryptoDonation.step2.label} */}
-            Send us an email...
-          </StyledStep>
-          <StyledStepDetails>with the transactions details, and we'll send you confirmation of your tax-deductible donation.</StyledStepDetails>
+            <StyledStep>
+              {/* {this.props.cryptoDonation.step2.label} */}
+              Send us an email...
+            </StyledStep>
+            <StyledStepDetails>with the transactions details, and we'll send you confirmation of your tax-deductible donation.</StyledStepDetails>
           </Box>
         </Flex>
         <Flex my={[3, 3, 4]} alignItems='baseline'>
           <StyledStepNumber>3.</StyledStepNumber>
           <Box>
-          <StyledStep>
-            {/* {this.props.cryptoDonation.step3.label} */}
-            Feel good...
+            <StyledStep>
+              {/* {this.props.cryptoDonation.step3.label} */}
+              Feel good...
           </StyledStep>
-          <StyledStepDetails>about supporting people with Alopecia!</StyledStepDetails>
+            <StyledStepDetails>about supporting people with Alopecia!</StyledStepDetails>
           </Box>
         </Flex>
       </div>
